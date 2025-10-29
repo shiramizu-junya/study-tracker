3e179faf2473f32d34238b3c9c95a2e09b4292de
Shiramizu_Junya <jyunya.pg.7231@gmail.com>
2025-10-29 22:14:15 +0900

    feat: 学習記録アプリの機能追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,71 @@
+import { useState } from 'react';
+
+const records = [
+	{ title: '勉強の記録1', time: 1 },
+	{ title: '勉強の記録2', time: 3 },
+	{ title: '勉強の記録3', time: 5 },
+];
+
 function App() {
+	const [learningContent, setLearningContent] = useState('');
+	const [learningTime, setLearningTime] = useState('0');
+	const [studyRecords, setStudyRecords] = useState(records);
+	const [isError, setIsError] = useState(false);
+
+	const totalStudyTime = studyRecords.reduce((total, record) => total + record.time, 0);
+
+	const handleRegistration = () => {
+		if (!learningContent || !learningTime || learningTime === '0') {
+			setIsError(true);
+		} else {
+			setIsError(false);
+			const newRecord = { title: learningContent, time: Number(learningTime) };
+			setStudyRecords([...studyRecords, newRecord]);
+			setLearningContent('');
+			setLearningTime('0');
+		}
+	};
+
 	return (
 		<>
 			<div>
 				<span>学習内容</span>
-				<input type="text" />
+				<input
+					type="text"
+					value={learningContent}
+					onChange={(e) => setLearningContent(e.target.value)}
+				/>
 			</div>
 			<div>
 				<span>学習時間</span>
-				<input type="number" />
+				<input
+					type="number"
+					min={0}
+					value={learningTime}
+					onChange={(e) => setLearningTime(e.target.value)}
+				/>
 				時間
 			</div>
 			<div>
-				<p>入力されている学習内容: お金, 勉強, プログラミング</p>
-				<p>入力されている学習時間: 0時間</p>
+				<p>入力されている学習内容: {learningContent}</p>
+				<p>入力されている学習時間: {learningTime}時間</p>
 			</div>
-      <div>
-        <button>登録</button>
-        <p>エラーメッセージ: 学習内容と学習時間を入力してください。</p>
-      </div>
+			<div>
+				<button onClick={handleRegistration}>登録</button>
+				<p>{isError && '入力されていない項目があります。'}</p>
+			</div>
 			<div>
 				<p>学習記録一覧</p>
 				<ul>
-					<li>お金 - 2時間</li>
-					<li>勉強 - 1時間</li>
-					<li>プログラミング - 3時間</li>
+					{studyRecords.map((record, index) => (
+						<li key={index}>
+							{record.title} - {record.time}時間
+						</li>
+					))}
 				</ul>
 			</div>
 			<div>
-				<p>合計学習時間: 0/1000 時間</p>
+				<p>合計学習時間: {totalStudyTime}/1000 時間</p>
 			</div>
 		</>
 	);
